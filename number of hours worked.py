916164de4e34450e47a01a3261e94f552dc998a1
Adel <ad127el@gmail.com>
2025-03-07 15:57:40 +0530

    Add files via upload

---
+++ number of hours worked.py
@@ -0,0 +1,15 @@
+hrs = input("Enter Hours: ") # Input is taken as a string
+hrs = float(hrs) # Convert the string input to a float
+
+
+# Input is taken as a string
+rate = input("Enter Rate: ") 
+
+# Convert the string input to a float
+rate = float(rate) 
+
+# Compute gross pay
+gross_pay = hrs * rate 
+
+# Display the result
+print(gross_pay)
